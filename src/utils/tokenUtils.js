206c4775b2c753c8bd6fb158fdf66bc655a779b9
Loris Caputo <46825967+loris1712@users.noreply.github.com>
2024-10-30 22:43:31 +0100

    Changes

--- src/utils/tokenUtils.js
+++ src/utils/tokenUtils.js
@@ -1,8 +1,18 @@
 // utils/tokenUtils.js
 export function getNicknameFromToken(token) {
   const validTokens = {
-      "test-token": { nickname: "User123", type: "vip" },
-      "regular-token": { nickname: "User456", type: "regular" },
+      "alexandrinamolinaro": { nickname: "Alexandrina Molinaro", type: "regular" },
+      "tommaso.santinii": { nickname: "Tommaso Santini", type: "regular" },
+      "loris.caputo": { nickname: "Loris Caputo", type: "vip" },
+      "_andreaspeziale_": { nickname: "Andrea Speziale", type: "regular" },
+      "Giuliaapolizzi": { nickname: "Giulia Polizzi", type: "regular" },
+      "giada.martinaa": { nickname: "Giada Martina Pici", type: "regular" },
+      "francescomarra23": { nickname: "Francesco Marra", type: "regular" },
+      "paolaarmillotta": { nickname: "Paola Armillotta", type: "regular" },
+      "giuliaabrambilla": { nickname: "Giulia Brambilla", type: "regular" },
+      "cele.baldassarre": { nickname: "Maria Celeste Baldassarre", type: "regular" },
+      "Machi_123": { nickname: "Maria chiara ghisolfi", type: "regular" },
+      "Ludobalestrucci": { nickname: "Ludovica Balestrucci", type: "regular" },
   };
   
   return validTokens[token] || null;
